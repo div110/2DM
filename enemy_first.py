93b544cfaee423bc524599e6da595b2ff87e0994
DavesCzFirst <davesczbusiness@gmail.com>
2025-05-05 17:43:33 +0200

    Create enemy_first.py

---
+++ enemy_first.py
@@ -0,0 +1,10 @@
+class Enemy:
+    def __init__(self, pos_x, pos_y, difficulty):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.difficulty = difficulty
+    
+    
+    
+    def enemy_movement(player_x, player_y):
+        pass
