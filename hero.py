2770fef4c4395803e6bf90b177a21d6b1ca45533
Matej Jasik <matej0264@gmail.com>
2025-05-06 09:34:00 +0200

    Update hero.py

--- hero.py
+++ hero.py
@@ -3,6 +3,9 @@ class hero():
         self.position_x : int = position_x
         self.position_y : int = position_y
         self.health_points : int = health_points
+        self.image : str = image
+        self.level : int = level
+        self.max_health : int = max_health
     
     def is_alive(self):
         if self.health_points <= 0:
